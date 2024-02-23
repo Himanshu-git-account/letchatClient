5a1864d18480234344feb2db033954ffc587bdb3
Kumar Himanshu <khimanshu0507@gmail.com>
2024-02-23 12:36:21 +0530

    LetsChat

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React,{useEffect} from 'react'
 import './App.css';
+import {io} from 'socket.io-client'
 
 function App() {
+  const socket = io("http://localhost:3000/")
+  useEffect(()=>{
+    socket.on("connect",()=>{
+      console.log("connected",socket.id)
+    })
+
+    socket.on("welcome",(data)=>{
+      console.log(data)
+    })
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     App
     </div>
   );
 }
